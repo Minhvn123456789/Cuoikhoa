47a83e7c09e572ae3b7789a401deb19de11211d2
Minhvn123456789 <aaaaaa110176@gmail.com>
2024-08-31 09:24:42 +0700

    Update practice.js

--- practice.js
+++ practice.js
@@ -1,9 +1,8 @@
-
 var CurrUser = localStorage.getItem("CurrUser")
 if (CurrUser) {
   var li = document.getElementById("Toggle")
   var tk = document.getElementById("tk")
-      tk.innerHTML = CurrUser
+      tk.innerHTML = CurrUser 
   let h5 = document.createElement("h5")
     h5.setAttribute("id","logOut")
     h5.innerHTML = "Đăng xuất"
@@ -17,45 +16,31 @@ if (CurrUser) {
     var li = document.getElementById("Toggle")
     li.innerHTML = '<a href="login.html">Đăng Ký</a><a href="Đn.html">/Đăng nhập</a>'
 }
+
 function addToCart(e) {
-
-  var name = e.parentNode.parentNode.children[1].children[0].innerHTML
-  var price = e.parentNode.children[0].innerHTML
-  var image = e.parentNode.parentNode.children[0].children[0]
-  var src = image.getAttribute('src')
-  var number = 0
+  var name = e.parentNode.parentNode.children[1].innerText;
+  var price = e.parentNode.children[0].innerText;
+  var image = e.parentNode.parentNode.children[0].children[0];
+  var src = image.getAttribute('src');
   var obj = {
-    name: name,
-    price: price,
-    image: src,
-    count: 1
-  }
-  if (localStorage.getItem('dsSP')) {
-    JSON.parse(localStorage.getItem("dsSP")).forEach((e, i) => {
-      if (e.name == name) {
-        var data = JSON.parse(localStorage.getItem("dsSP"))
-        var count = data[i].count
-        console.log(count)
-        data[i].count = count + 1
-        console.log(data)
-        localStorage.setItem("dsSP", JSON.stringify(data))
-        return
-      }
-      number++
-    });
-    if (number == JSON.parse(localStorage.getItem("dsSP")).length) {
-      var lstSP = JSON.parse(localStorage.getItem('dsSP'))
-      lstSP.push(obj)
-      localStorage.setItem('dsSP', JSON.stringify(lstSP))
-    }
+      name: name,
+      price: price,
+      image: src,
+      count: 1
+  };
+ 
 
+  var cart = JSON.parse(localStorage.getItem('dsSP')) || [];
+  var itemIndex = cart.findIndex(item => item.name === name);
+  if (itemIndex !== -1) {
+      cart[itemIndex].count += 1;
+  } else {
+      cart.push(obj);
+      alert("Da them vao gio hang");
   }
-  else {
-    lstSP = []
-    lstSP.push(obj)
-    localStorage.setItem('dsSP', JSON.stringify(lstSP))
-  }
+  localStorage.setItem('dsSP', JSON.stringify(cart));
 }
+
 function addToYT(e) {
   var name = e.parentNode.parentNode.children[1].innerText;
   var price = e.parentNode.children[0].innerText;
@@ -73,6 +58,7 @@ function addToYT(e) {
   var itemIndex = favorites.findIndex(item => item.name === name);
   if (itemIndex === -1) {
       favorites.push(favoriteItem);
+      alert("Da them vao phan yeu thich");
   }
 
   localStorage.setItem('favorites', JSON.stringify(favorites));
